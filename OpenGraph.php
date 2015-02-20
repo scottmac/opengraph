cc30c6dded12394fcee72fc32cd3a82373544fc3
Danilo Ramírez <daniloramirez.cr@gmail.com>
2015-02-20 08:12:10 -0600

    Merging changes from @danilor to support cURL cookies
    This will require more testing, but I think it is a great idea if it
    works.

--- OpenGraph.php
+++ OpenGraph.php
@@ -13,9 +13,9 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-   
+
 	Original can be found at https://github.com/scottmac/opengraph/blob/master/OpenGraph.php
-   
+
 */
 
 class OpenGraph implements Iterator
@@ -50,25 +50,28 @@ class OpenGraph implements Iterator
    * @return OpenGraph
    */
 	static public function fetch($URI) {
-        $curl = curl_init($URI);
+    $curl = curl_init($URI);
 
-        curl_setopt($curl, CURLOPT_FAILONERROR, true);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 15);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_USERAGENT, "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)");
+    curl_setopt($curl, CURLOPT_FAILONERROR, true);
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 15);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_USERAGENT, "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)");
+    //The following 2 set up lines work with sites like www.nytimes.com
+    curl_setopt($curl, CURLOPT_COOKIEFILE, "cookie.txt"); //you can change this path to whetever you want.
+    curl_setopt($curl, CURLOPT_COOKIEJAR, "cookie.txt"); //you can change this path to whetever you want.
 
-        $response = mb_convert_encoding(curl_exec($curl), 'HTML-ENTITIES', 'UTF-8');
+    $response = mb_convert_encoding(curl_exec($curl), 'HTML-ENTITIES', 'UTF-8');
 
-        curl_close($curl);
+    curl_close($curl);
 
-        if (!empty($response)) {
-            return self::_parse($response);
-        } else {
-            return false;
-        }
+    if (!empty($response)) {
+        return self::_parse($response);
+    } else {
+        return false;
+    }
 	}
 
   /**
@@ -83,7 +86,7 @@ class OpenGraph implements Iterator
 
 		$doc = new DOMDocument();
 		$doc->loadHTML($HTML);
-		
+
 		libxml_use_internal_errors($old_libxml_error);
 
 		$tags = $doc->getElementsByTagName('meta');
@@ -94,15 +97,15 @@ class OpenGraph implements Iterator
 		$page = new self();
 
 		$nonOgDescription = null;
-		
+
 		foreach ($tags AS $tag) {
 			if ($tag->hasAttribute('property') &&
 			    strpos($tag->getAttribute('property'), 'og:') === 0) {
 				$key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
 				$page->_values[$key] = $tag->getAttribute('content');
 			}
-			
-			//Added this if loop to retrieve description values from sites like the New York Times who have malformed it. 
+
+			//Added this if loop to retrieve description values from sites like the New York Times who have malformed it.
 			if ($tag ->hasAttribute('value') && $tag->hasAttribute('property') &&
 			    strpos($tag->getAttribute('property'), 'og:') === 0) {
 				$key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
@@ -112,7 +115,7 @@ class OpenGraph implements Iterator
 			if ($tag->hasAttribute('name') && $tag->getAttribute('name') === 'description') {
                 $nonOgDescription = $tag->getAttribute('content');
             }
-			
+
 		}
 		//Based on modifications at https://github.com/bashofmann/opengraph/blob/master/src/OpenGraph/OpenGraph.php
 		if (!isset($page->_values['title'])) {
@@ -140,7 +143,7 @@ class OpenGraph implements Iterator
         }
 
 		if (empty($page->_values)) { return false; }
-		
+
 		return $page;
 	}
 
@@ -155,7 +158,7 @@ class OpenGraph implements Iterator
 		if (array_key_exists($key, $this->_values)) {
 			return $this->_values[$key];
 		}
-		
+
 		if ($key === 'schema') {
 			foreach (self::$TYPES AS $schema => $types) {
 				if (array_search($this->_values['type'], $types)) {
@@ -192,7 +195,7 @@ class OpenGraph implements Iterator
 		if (array_key_exists('latitude', $this->_values) && array_key_exists('longitude', $this->_values)) {
 			return true;
 		}
-		
+
 		$address_keys = array('street_address', 'locality', 'region', 'postal_code', 'country_name');
 		$valid_address = true;
 		foreach ($address_keys AS $key) {
